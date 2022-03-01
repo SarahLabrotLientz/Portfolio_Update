838d015ce35af1461f37a654c023813d7dbe8419
Labrot <sarah.lientz@gmail.com config --global user.email>
2022-03-01 12:36:52 -0500

    added javascript animation

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+const changingKeywords = document.querySelectorAll("span.changing-keyword");
+const keywordsToggle = setKeywordsToggle(changingKeywords);
+
+function setKeywordsToggle(keywords) {
+  let index = 0;
+  return setInterval(() => {
+    keywords[index].classList.remove("shown");
+    if (++index >= keywords.length) index = 0;
+    keywords[index].classList.add("shown");
+  }, 2000);
+}
 
 anime({
-    targets: 'div',
-    translateX: 250,
-    rotate: '1turn',
-    backgroundColor: '#FFF',
-    duration: 800
-  });
+  targets: "div",
+  translateX: 250,
+  rotate: "1turn",
+  backgroundColor: "#FFF",
+  duration: 800,
+});
